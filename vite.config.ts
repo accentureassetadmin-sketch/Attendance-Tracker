083f877cbe2b70d5e8c404f536098e76a6e4df9c
accentureassetadmin-sketch <accentureassetadmin@gmail.com>
2026-05-03 22:46:24 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  // This must match your GitHub repository name exactly
+  base: '/Attendance-Tracker/', 
+  plugins: [react()],
 })
